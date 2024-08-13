1e2911848e26440e3de481a2a82cfdec1c125e48
P.Leshchanka <pavel.planer@gmail.com>
2024-08-13 14:03:44 +0300

    created git teg creator, with reading from console

---
+++ src/main/kotlin/ru/clevertec/GitTagVersionCreatorPlugin.kt
@@ -0,0 +1,70 @@
+package ru.clevertec
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import ru.clevertec.util.GitChecker
+import ru.clevertec.util.GitListener
+import ru.clevertec.util.TagParser
+
+private const val MASTER = "master"
+private const val STAGE = "stage/"
+private const val DEV = "dev/"
+private const val QA = "qa/"
+
+class GitTagVersionCreatorPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+
+        project.tasks.register("createTag") { task ->
+            task.group = "git tag version creator"
+            task.doFirst() {
+                if (!GitChecker().hasTagOnCommit()) {
+                    val gitListener = GitListener()
+                    gitListener.runGitFetch()
+                    val majorNumberOfGreatestVersion = TagParser().getMajorNumberOfGreatestVersion()
+                    val minorNumberOfGreatestVersion = TagParser().getMinorNumberOfGreatestVersion()
+                    val branch = gitListener.getGitCurrentBranch()
+                    val tag: String
+                    when {
+                        branch.contains(MASTER) -> {
+                            tag = String.format(
+                                "v%d.0",
+                                (majorNumberOfGreatestVersion + 1)
+                            )
+                        }
+
+                        branch.contains(STAGE) -> {
+                            tag = String.format(
+                                "v%d.%d-rc",
+                                majorNumberOfGreatestVersion,
+                                (minorNumberOfGreatestVersion + 1)
+                            )
+                        }
+
+                        branch.contains(DEV) || branch.contains(QA) -> {
+                            tag = String.format(
+                                "v%d.%d",
+                                majorNumberOfGreatestVersion,
+                                (minorNumberOfGreatestVersion + 1)
+                            )
+                        }
+
+                        else -> tag = "v$majorNumberOfGreatestVersion.$minorNumberOfGreatestVersion-SNAPSHOT"
+
+                    }
+
+                    if (!GitChecker().hasModifiedFiles() && !GitChecker().hasUntrackedFiles()) {
+                        println(tag)
+                        gitListener.createGitTag(tag)
+                        gitListener.runGitPush()
+                    } else {
+                        println("$tag.uncommitted")
+                    }
+                } else {
+                    println("not commited")
+                    println("commit has tag")
+                }
+            }
+        }
+    }
+}
