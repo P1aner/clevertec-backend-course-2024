97e4ca0d1cdf263dcae754210c64fa8dee937a37
Pavel Leshchanka <pavel.leshchanka@gmail.com>
2024-09-24 18:08:49 +0300

    fix method of topics

--- src/main/java/ru/clevertec/manager/TopicManager.java
+++ src/main/java/ru/clevertec/manager/TopicManager.java
@@ -5,32 +5,22 @@ import java.util.Map;
 import java.util.Optional;
 
 public class TopicManager implements Manager {
-    private static volatile TopicManager instance;
     private final Map<String, Topic> topics;
 
     private TopicManager(Map<String, Topic> topics) {
         this.topics = topics;
     }
 
+    private static class TopicManagerSingletonHandler {
+        private static final TopicManager instance = new TopicManager(new HashMap<>());
+    }
+
     public static TopicManager getInstance() {
-        TopicManager localInstance = instance;
-        if (localInstance == null) {
-            synchronized (TopicManager.class) {
-                localInstance = instance;
-                if (localInstance == null) {
-                    instance = localInstance = new TopicManager(new HashMap<>());
-                }
-            }
-        }
-        return localInstance;
+        return TopicManagerSingletonHandler.instance;
     }
 
     public Topic createTopic(String name, int maxConsumers) {
-        Topic topic = topics.get(name);
-        if (topic == null) {
-            topics.put(name, new Topic(name, maxConsumers));
-        }
-        return topics.get(name);
+        return topics.computeIfAbsent(name, k -> new Topic(name, maxConsumers));
     }
 
     public Optional<Topic> getTopic(String name) {
