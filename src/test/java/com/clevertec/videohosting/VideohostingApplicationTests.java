4bf3c36f926f7e613205aedd25630189dcd88c0e
Pavel Leshchanka <pavel.leshchanka@gmail.com>
2024-10-07 00:11:28 +0300

    init project

---
+++ src/test/java/com/clevertec/videohosting/VideohostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clevertec.videohosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideohostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
